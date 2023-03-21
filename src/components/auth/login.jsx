fe3ac263e3d61e6b241faf9a699816db12f85266
juank2808 <webjcaf022014@gmail.com>
2023-03-21 21:43:20 +0100

    login en proceso

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,5 +1,16 @@
 
+import { login } from "../../data/api";
+import { useState } from "react";
 const Login = () => {
+    const [data, setData] = useState({});
+    const handleChange = async (evt) => {
+        console.log(evt.target.value);
+        setData({...data,[evt.target.name]:evt.target.value});
+    }
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        login(data);
+    }   
     return(
         <>
             <main>
@@ -12,11 +23,12 @@ const Login = () => {
                                     </ul>
                                     <div className="tab-content">
                                         <div className="tab-pane fade show active" role="tabpanel" aria-labelledby="tab-login">
-                                            <form>
+                                            <form action=""> 
                                                 <h1>KronumApp</h1>
+                                                <br/>
                                                 <div className="form-outline">
-                                                    <input className="form-control mb-4" id="loginName" type="email" value=""/>
-                                                    <label className="form-label" for="loginName">Email or username</label>
+                                                    <input name="email" className="form-control mb-4" id="loginName" type="email"  onChange={(evt)=>handleChange(evt)}/>
+                                                    <label className="form-label" for="loginName">Email</label>
                                                     <div className="form-notch">
                                                         <div className="form-notch-leading"></div>
                                                         <div className="form-notch-middle" ></div>
@@ -24,15 +36,15 @@ const Login = () => {
                                                     </div>
                                                 </div>
                                                 <div className="form-outline">
-                                                    <input className="form-control mb-4" id="loginPassword" type="password" value=""/>
-                                                    <label className="form-label" for="loginPassword">Password</label>
+                                                    <input name="password" className="form-control mb-4" id="loginPassword" type="password" onChange={(evt)=>handleChange(evt)}/>
+                                                    <label className="form-label" for="loginPassword">Contraseña</label>
                                                     <div className="form-notch">
                                                         <div className="form-notch-leading"></div>
                                                         <div className="form-notch-middle" ></div>
                                                         <div className="form-notch-trailing"></div>
                                                     </div>
                                                 </div>
-                                                <button className="ripple ripple-surface btn btn-primary btn-block mb-4" type="submit" role="button">Iniciar sesión</button>
+                                                <button className="ripple ripple-surface btn btn-primary btn-block mb-4" onClick={(evt)=>{handleSubmit(evt)}}>Iniciar sesión</button>
                                             </form>
                                         </div>
                                     </div>
